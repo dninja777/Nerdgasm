4f7699a82a8fd1d1414cad08c83c969a6e5f548a
Duron Epps <duroneppsjr@hotmail.com>
2017-02-15 06:55:35 +0000

    show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'welcome#welcome'
-  
+  resources :pics
   resources :users do
     resource :profile
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215055653) do
+ActiveRecord::Schema.define(version: 20170215063120) do
+
+  create_table "pics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
